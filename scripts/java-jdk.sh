cd617c7ec23d2a156521a5d4e8a72f937392dcba
Marco Maccio <marco.maccio@marmac.name>
2016-09-09 22:06:43 +0200

    Changed the -y flag position

--- scripts/java-jdk.sh
+++ scripts/java-jdk.sh
@@ -2,4 +2,4 @@
 echo "***** JAVA JDK installation"
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+sudo apt-get install -y oracle-java8-installer
