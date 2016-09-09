f88ac61870e00d1831316af9106c52866fb6d026
Marco Maccio <marco.maccio@marmac.name>
2016-09-09 17:01:40 +0200

    Draft a sh script to build all the images

---
+++ package-all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+echo "IMAGE CREATION PART ..."
+
+echo "Packaging Jenkins Server..."
+
+echo "Jenkins Server Packaged !"
+
+echo "Packaging Agilefant Server..."
+
+echo "Agilefant Server Packaged !"
+
+echo "Packaging Gitlab Server..."
+
+echo "Gitlab Server Packaged !"
+
+echo "Packaging Nexus Server..."
+
+echo "Nexus Server Packaged !"
+
+echo "Packaging Sonarqube Server..."
+
+echo "Sonarqube Server Packaged !"
+
+echo "Packaging NGINX Server..."
+
+echo "NGINX Server Packaged !"
+
+echo "IMAGE CREATION PART ended successfully !"
+
+echo "VM CREATION PART ..."
+
+echo "VM CREATION PART ended successfully !"
