66b7d2473e6da671d4cc1dcbf62f4551d0c36af9
Marco Maccio <marco.maccio@marmac.name>
2018-05-22 09:28:05 +0200

    Eliminated old file to add the newer version from the old devpaas repo

--- scripts/jenkins/graphviz.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-echo "***** GRAPHVIZ installation"
-sudo apt-get install -y graphviz
--- scripts/nginx/nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-echo "***** Starting NGINX configuration"
-sudo sed -i 's/~~JENKINS_IP~~/'$JENKINS_IP'/' /tmp/resources/default.conf
-sudo cp /tmp/default.conf /etc/nginx/sites-enabled/default
-sudo service nginx reload
