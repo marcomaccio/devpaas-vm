8220ffe890752eaea668564a99340aa9c4a2f1cb
Marco Maccio <marco.maccio@marmac.name>
2018-06-04 17:23:26 +0200

    Added verification of dir localhost before moving the serverspec files

--- packer/scripts/serverspec/serverspec-mongodb.sh
+++ packer/scripts/serverspec/serverspec-mongodb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 
+if [ ! -d "/tmp/serverspec/spec/localhost" ]; then
+  mkdir /tmp/serverspec/spec/localhost
+fi
+
 echo "***** Copying Serverspec Tests x MongoDB *****"
-
 cp /tmp/mongodb/tests/*.rb   /tmp/serverspec/spec/localhost/
-
--- packer/scripts/serverspec/serverspec-sonarqube.sh
+++ packer/scripts/serverspec/serverspec-sonarqube.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
-echo "***** Copying Serverspec Tests x Sonarqube *****"
+if [ ! -d "/tmp/serverspec/spec/localhost" ]; then
+  mkdir /tmp/serverspec/spec/localhost
+fi
 
+echo "***** Copying Serverspec Tests x Sonarqube *****"
 cp /tmp/sonarqube/tests/*.rb /tmp/serverspec/spec/localhost/
