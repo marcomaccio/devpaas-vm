1a14aed22b316b077125f2b6f8cd08b4cdb58f37
Marco Maccio <marco.maccio@marmac.name>
2018-06-06 11:08:24 +0200

    Made uniform the 3 templates for base, node, jenkins on ubuntu srv 1604

--- packer/VBOX-build-image-devpaas-base-ubuntu-server-1604.sh
+++ packer/VBOX-build-image-devpaas-base-ubuntu-server-1604.sh
@@ -8,40 +8,31 @@
 # description:    Create image for Base Image server based on Ubuntu 16.04 ISO image
 #
 # to run:
-#           sh VBOX-build-image-devpaas-base-ubuntu-server-1604.sh virtualbox-iso           \
-#                                                               -debug                      \
-#                                                               IMAGE_ISO_URL               \
-#                                                               IMAGE_ISO_CHECKSUM          \
-#                                                               IMAGE_ISO_CHECKSUM_TYPE     \
-#                                                               IMAGE_OUTPUT_NAME           \
-#                                                               IMAGE_OUTPUT_VERSION        \
-#                                                               VBOX_VERSION                \
-#                                                               VBOX_GUEST_ADDITIONS_URL    \
-#                                                               VBOX_SSH_USER_FULL_NAME     \
-#                                                               VBOX_SSH_USERNAME           \
-#                                                               VBOX_SSH_PASSWORD           \
-#                                                               VBOX_OVA_SOURCE_PATH        \
-#                                                               PRESEED_FILENAME            \
-#                                                               INSTANCE_VCPU               \
-#                                                               INSTANCE_MEMORY             \
+#           sh VBOX-build-image-devpaas-base-ubuntu-server-1604.sh virtualbox-iso               \
+#                                                               -debug                          \
+#                                                               IMAGE_ISO_URL                   \
+#                                                               IMAGE_ISO_CHECKSUM              \
+#                                                               IMAGE_ISO_CHECKSUM_TYPE         \
+#                                                               IMAGE_OUTPUT_NAME               \
+#                                                               IMAGE_OUTPUT_VERSION            \
+#                                                               VBOX_VERSION                    \
+#                                                               VBOX_GUEST_ADDITIONS_URL        \
+#                                                               VBOX_DISK_SIZE                  \
+#                                                               VBOX_SSH_USER_FULL_NAME         \
+#                                                               VBOX_SSH_USERNAME               \
+#                                                               VBOX_SSH_PASSWORD               \
+#                                                               VBOX_SHARED_FOLDER_ROOT_PATH    \
+#                                                               VBOX_OVA_SOURCE_PATH            \
+#                                                               PRESEED_FILENAME                \
+#                                                               INSTANCE_VCPU                   \
+#                                                               INSTANCE_MEMORY                 \
 #                                                               INSTANCE_DOMAIN_NAME
 ################################################################################################
 
 SECONDS=0
-echo " Start: " `date`
+echo "Start: " `date`
 
-export PACKER_LOGS_FILENAME="VBOX-packer-base-ubuntu-server-1604.log"
-
-mkdir -p builds/packer_logs
-echo "Verify if already exist the packer log file"
-if [ -e ./builds/packer_logs/$PACKER_LOGS_FILENAME ]
-then
-    echo "Delete the log file of a previous run ..."
-    rm -f ./builds/packer_logs/$PACKER_LOGS_FILENAME
-fi
-
-export PACKER_LOG=1
-export PACKER_LOG_PATH="./builds/packer_logs/$PACKER_LOGS_FILENAME"
+echo "Reading script parameters ... "
 
 export PACKER_PROVIDERS_LIST=${1}           # ex. virtualbox-iso or virtualbox-ovf
 
@@ -50,7 +41,7 @@ export DEBUG=${2}                           # ex. -debug
 export IMAGE_ISO_URL=${3}                   # ex. http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-amd64.iso
 export IMAGE_ISO_CHECKSUM=${4}              # ex. 0a03608988cfd2e50567990dc8be96fb3c501e198e2e6efcb846d89efc7b89f2
 export IMAGE_ISO_CHECKSUM_TYPE=${5}         # ex. sha256
-export IMAGE_OUTPUT_NAME=${6}               # ex. base-ubuntu-server-1604
+export IMAGE_OUTPUT_NAME=${6}               # ex. base-ubu-srv-1604
 export IMAGE_OUTPUT_VERSION=${7}            # ex. 0.0.1
 
 export VBOX_VERSION=${8}                    # ex. 5.2.8
@@ -59,7 +50,7 @@ export VBOX_DISK_SIZE=${10}                 # ex. 92160 = 90 Gb
 export VBOX_SSH_USER_FULL_NAME=${11}        # ex. Administrator
 export VBOX_SSH_USERNAME=${12}              # ex. packer
 export VBOX_SSH_PASSWORD=${13}              # ex. packer
-export VBOX_SHARED_FOLDER_ROOT_PATH=${14}   # ex. /c/deployments
+export VBOX_SHARED_FOLDER_ROOT_PATH=${14}   # ex. vm-deployments
 export VBOX_OVA_SOURCE_PATH=${15}           # ex. deployment/mm-base-ubuntu-1604-0.0.1/mm-base-ubuntu-1604-0.0.1.ovf
 
 export PRESEED_FILENAME=${16}               # ex. preseed-base-ubuntu-server-1604.cfg
@@ -68,13 +59,31 @@ export INSTANCE_VCPU=${17}                  # ex. 2
 export INSTANCE_MEMORY=${18}                # ex. 1024 = 1Gb
 export INSTANCE_DOMAIN_NAME=${19}           # ex. domain-name.local
 
-export INSTANCE_NAME="${IMAGE_OUTPUT_NAME}-${IMAGE_OUTPUT_VERSION}"
-echo "$INSTANCE_NAME"
+export INSTANCE_NAME="${IMAGE_OUTPUT_NAME}-$(($RANDOM%100))"
+echo "Instance Name ${INSTANCE_NAME}, that will be created in ${PACKER_PROVIDERS_LIST}"
+
+echo "Creating packer log file name ..."
+export PACKER_LOGS_FILENAME="VBOX-${IMAGE_OUTPUT_NAME}-${IMAGE_OUTPUT_VERSION}.log"
+
+mkdir -p builds/packer_logs
+echo "Verify if already exist the packer log file"
+if [ -e ./builds/packer_logs/$PACKER_LOGS_FILENAME ]
+then
+    echo "Delete the log file of a previous run ..."
+    rm -f ./builds/packer_logs/$PACKER_LOGS_FILENAME
+fi
+export PACKER_LOG=1
+export PACKER_LOG_PATH="./builds/packer_logs/$PACKER_LOGS_FILENAME"
+echo "The packer log file is available at ${PACKER_LOG_PATH} ..."
 
 echo "Preparing build for $PACKER_PROVIDERS_LIST ..."
 export PACKER_TEMPLATE=templates/packer-devpaas-base-ubuntu-server-1604.json
 
-echo "Build the preseed file from the template ..."
+echo "Create Shared directory on the host to be mounted on the guest VM ..."
+export SHARED_FOLDER_ABS_PATH=${HOME}/${VBOX_SHARED_FOLDER_ROOT_PATH}/${IMAGE_OUTPUT_NAME}/data
+
+echo "Absolute Path for the shared folder: ${SHARED_FOLDER_ABS_PATH}"
+mkdir -p ${SHARED_FOLDER_ABS_PATH}
 
 echo "Create the preseed ${PRESEED_FILENAME} file from the template ..."
 sed "s/HOST_NAME/${INSTANCE_NAME}/g; s/DOMAIN_NAME/${INSTANCE_DOMAIN_NAME}/g" http/preseed-ubuntu-server-1604.template > http/${PRESEED_FILENAME}
@@ -101,7 +110,7 @@ packer validate -only=${PACKER_PROVIDERS_LIST}                              \
         -var "vbox_ssh_username=${VBOX_SSH_USERNAME}"                       \
         -var "vbox_ssh_password=${VBOX_SSH_PASSWORD}"                       \
         -var "vbox_ova_source_path=${VBOX_OVA_SOURCE_PATH}"                 \
-        -var "vbox_shared_folder_root_path=${VBOX_SHARED_FOLDER_ROOT_PATH}" \
+        -var "vbox_shared_folder_abs_path=${SHARED_FOLDER_ABS_PATH}" \
         -var "preseed_filename=${PRESEED_FILENAME}"                         \
         ${PACKER_TEMPLATE}
 
@@ -121,7 +130,7 @@ packer build -force -on-error=abort -only=${PACKER_PROVIDERS_LIST}  ${DEBUG}    
         -var "vbox_ssh_username=${VBOX_SSH_USERNAME}"                       \
         -var "vbox_ssh_password=${VBOX_SSH_PASSWORD}"                       \
         -var "vbox_ova_source_path=${VBOX_OVA_SOURCE_PATH}"                 \
-        -var "vbox_shared_folder_root_path=${VBOX_SHARED_FOLDER_ROOT_PATH}" \
+        -var "vbox_shared_folder_abs_path=${SHARED_FOLDER_ABS_PATH}" \
         -var "preseed_filename=${PRESEED_FILENAME}"                         \
         ${PACKER_TEMPLATE}
 
