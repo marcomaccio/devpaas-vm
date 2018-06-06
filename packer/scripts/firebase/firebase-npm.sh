b1d6e29f7d45c75f395a5bfc4c945a79b25a1994
Marco Maccio <marco.maccio@marmac.name>
2018-06-06 16:20:51 +0200

    Added scripts to install Firebase tools via yarn & npm

---
+++ packer/scripts/firebase/firebase-npm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+echo "**************************************************"
+echo "***** Start installation of Firebase via npm *****"
+
+sudo npm install -g firebase-tools
+
+echo "***** installation of Firebase via npm Ended *****"
+echo "**************************************************"
---
+++ packer/scripts/firebase/firebase-yarn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+echo "***************************************************"
+echo "***** Start installation of Firebase via yarn *****"
+
+sudo yarn global add firebase-tools
+
+echo "***** installation of Firebase via yarn Ended *****"
+echo "***************************************************"
