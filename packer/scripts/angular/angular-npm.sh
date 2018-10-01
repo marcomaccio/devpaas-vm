05c6ba1481817cdddfbf1180fa0fcdfdc444dd87
Marco Maccio <marco.maccio@marmac.name>
2018-10-01 17:39:27 +0200

    [Packer]-[VBOX]-[Node]: Eliminated the yarn config as it's a proj level

--- packer/scripts/angular/angular-npm.sh
+++ packer/scripts/angular/angular-npm.sh
@@ -5,7 +5,7 @@ echo "***** Start installation of Angular CLI via npm *****"
 
 sudo npm install -g @angular/cli
 
-ng config -g cli.packageManager yarn
+# ng config -g cli.packageManager yarn
 
 echo "***** installation of Angular CLI via npm Ended *****"
 echo "*****************************************************"
