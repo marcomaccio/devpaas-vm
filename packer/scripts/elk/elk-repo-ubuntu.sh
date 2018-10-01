9deb62cce7090c9430be9c60b9b9da44473e426c
Marco Maccio <marco.maccio@marmac.name>
2018-10-01 17:38:28 +0200

    [Packer]-[VBOX]-[ELK]: Updated the ELK template to adhere the standard

--- packer/scripts/elk/elk-repo-ubuntu.sh
+++ packer/scripts/elk/elk-repo-ubuntu.sh
@@ -6,6 +6,5 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo 'installing apt-transport-https package'
 sudo apt-get install apt-transport-https
 
-echo 'Save the repository definition to /etc/apt/sources.list.d/elastic-5.x.list'
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-
+echo 'Save the repository definition to /etc/apt/sources.list.d/elastic-6.x.list'
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
