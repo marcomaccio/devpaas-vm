258a213d874f8a0685f6e4834f4580e833c94d2d
marcomaccio <marco.maccio@marmac.name>
2018-11-06 08:46:00 +0100

    [Packer]-[NODEJS]-[Firebase]: no installation as should be in prj

--- packer/scripts/firebase/firebase-npm.sh
+++ packer/scripts/firebase/firebase-npm.sh
@@ -3,7 +3,7 @@
 echo "**************************************************"
 echo "***** Start installation of Firebase via npm *****"
 
-sudo npm install -g firebase-tools firebase
+sudo npm install -g firebase-tools
 
 echo "***** installation of Firebase via npm Ended *****"
 echo "**************************************************"
